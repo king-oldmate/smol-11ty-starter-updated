13202fc51d7e67bc49e56f2fbd84be46b272c186
king-oldmate <49798957+king-oldmate@users.noreply.github.com>
2024-12-06 21:31:47 +1100

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,11 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/css/");
   eleventyConfig.addWatchTarget("./src/css/");
+  eleventyConfig.addPassthroughCopy("./src/js/");
+  eleventyConfig.addWatchTarget("./src/js/");
+
+  eleventyConfig.addPassthroughCopy({ "src/images": "images" });
+  eleventyConfig.addWatchTarget("./src/css/");
 
   eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
 
